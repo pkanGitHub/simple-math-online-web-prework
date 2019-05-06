d463aae440b1d7f2c6ba2c9e1553f7447e6a476a
Phyllis Kan <pyk9q7@gmail.com>
2019-05-06 21:40:31 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -7,22 +7,22 @@ def subtraction(num1, num2)
 end
 
 def division(num1, num2)
-  puts (50 / 2)
+  puts (50 / 2).to_i
 end
 
 def multiplication(num1, num2)
-  puts (4 * 30)
+  puts (4 * 30).to_i
 end
 
 def modulo(num1, num2)
-  puts (34 % 5)
+  puts (34 % 5).to_i
 end
 
 def square_root(num)
-  puts Math.sqrt(81)
+  puts Math.sqrt(81).to_i
 end
 
 def order_of_operation(num1, num2, num3, num4)
   #Hint:  __ + (( __ * __ ) / __ )
-  puts (7 + ((43 * 23)/ 83))
+  puts (7 + ((43 * 23)/ 83)).to_i
 end
