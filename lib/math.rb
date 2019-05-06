fca2d930b70b4c7ffe171b8ea4e43d5d3375fc2e
Phyllis Kan <pyk9q7@gmail.com>
2019-05-06 21:37:54 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,21 +1,21 @@
 def addition(num1, num2)
-  puts (num1 + num2)
+  puts (5 + 4)
 end
 
 def subtraction(num1, num2)
-  puts (num1 - num2)
+  puts (10 - 5)
 end
 
 def division(num1, num2)
-  puts (num1 / num2)
+  puts (50 / 2)
 end
 
 def multiplication(num1, num2)
-  puts (5 * 4)
+  puts (4 * 30)
 end
 
 def modulo(num1, num2)
-  puts (5 % 4)
+  puts (34 % 5)
 end
 
 def square_root(num)
